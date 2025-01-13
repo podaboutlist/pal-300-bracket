31a423651249a24fea1697e0ec8449a4b082d6cb
Audrey <6566104+RalphORama@users.noreply.github.com>
2025-01-13 01:26:58 -0500

    v0.0.1

--- index.js
+++ index.js
@@ -22,7 +22,18 @@ let g_bracketStructure;
 		// seedID will be -1 for undeclared winner slots etc.
 		if (seedID > 0) {
 			const ed = getEpisodeDataFromSeed(seedID);
-			bis.innerHTML = `<a href="${ed['link']}" target="_blank" rel="noopener">${ed['title']}</a>`;
+
+			if (ed["link"] === "") {
+				bis.innerText = `${ed['title']}`
+			} else {
+				const a = document.createElement("a");
+				a.href = ed["link"];
+				a.target = "_blank";
+				a.rel = "noopener";
+				a.innerText = ed["title"];
+
+				bis.appendChild(a);
+			}
 		} else {
 			bis.innerText = "tbd";
 		}
@@ -32,8 +43,8 @@ let g_bracketStructure;
 		return bi;
 	}
 
-	function buildBracketStructure(roundData) {
-		console.debug("roundData is ", roundData);
+	function buildBracketStructure(roundData, roundID) {
+		console.debug("[buildBracketStructure]", "roundID is", roundID, "roundData is", roundData);
 
 		// I'm sure I could template all this crap but idc (^:
 		const group = document.createElement("div");
@@ -57,17 +68,17 @@ let g_bracketStructure;
 
 		// deepest level is an array of seedIDs
 		if (Array.isArray(roundData["entries"])) {
-			console.debug("buildBracketStructure: building entries from seedID array", roundData["entries"]);
+			console.debug("[buildBracketStructure]", "building entries from seedID array", roundData["entries"]);
 
 			roundData["entries"].forEach((seedID) => {
 				entries.appendChild(createBracketItem(seedID));
 			});
 		} else {
-			console.debug("buildBracketStructure: recursing down the tree!");
+			console.debug("[buildBracketStructure]", "recursing down the tree!");
 
 			// recurse down this mf!
 			for (let [k, v] of Object.entries(roundData["entries"])) {
-				const bs = buildBracketStructure(v);
+				const bs = buildBracketStructure(v, k);
 
 				bs.setAttribute("id", `round-${k}`);
 
@@ -75,10 +86,19 @@ let g_bracketStructure;
 			}
 		}
 
+		// TODO: Add background color to highlight winning entries?
+
 		group.appendChild(entries);
 		group.appendChild(winnerWrapper);
 
 		return group;
+	}
+
+	function populateOverallWinner(seedID) {
+		const elem = document.querySelector("#overall-winner > h2");
+		const epData = getEpisodeDataFromSeed(seedID);
+
+		elem.innerHTML = `<a href="${epData['link']}" target="_blank" rel="noopener">${epData['title']}</a>`;
 	}
 
 	// Pass these options to fetch() calls so they work with <link rel="preload" as="fetch"> from the HTML
@@ -89,10 +109,10 @@ let g_bracketStructure;
 		mode: 'no-cors',
 	}
 
-	const episodeData = await (await window.fetch("data/episodes.json", fetchOpts)).json();
 	const bracketStructure = await (await window.fetch("data/structure.json", fetchOpts)).json();
 
 	// for testing
+	const episodeData = await (await window.fetch("data/episodes.min.json", fetchOpts)).json();
 	g_episodeData = episodeData;
 	g_bracketStructure = bracketStructure;
 
@@ -105,9 +125,15 @@ let g_bracketStructure;
 	const lbc = document.querySelector("#left");
 	const rbc = document.querySelector("#right");
 
-	console.log("Populating left side of the bracket...");
-	lbc.appendChild(buildBracketStructure(leftBracketData));
+	console.log("[main]", "Populating left side of the bracket...");
+	lbc.appendChild(buildBracketStructure(leftBracketData, "139"));
 
-	console.log("Populating right side of the bracket...");
-	rbc.appendChild(buildBracketStructure(rightBracketData));
+	console.log("[main]", "Populating right side of the bracket...");
+	rbc.appendChild(buildBracketStructure(rightBracketData, "140"));
+
+	// HACK: This shouldn't be hardcoded lol
+	const overallWinnerSID = bracketStructure["141"]["winner"]
+	if (overallWinnerSID > 0) {
+		populateOverallWinner(overallWinnerSID);
+	}
 })();
