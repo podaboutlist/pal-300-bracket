ffd0317b6c09c10c68a46c36423e9b173d2f8cda
Audrey <6566104+RalphORama@users.noreply.github.com>
2025-01-16 02:05:34 -0500

    make the bot more better

--- bot/bot.py
+++ bot/bot.py
@@ -3,115 +3,97 @@
 import asyncio
 import discord
 import json
+import logging
 import os
 import sys
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
-from typing import Optional
 
 from cogs.ping import Ping
+from cogs.round_info import RoundInfo
 
 
 load_dotenv()
 
+discord.utils.setup_logging()
+logger = logging.getLogger(__name__)
+# TODO: Write logic to control this with os.getenv('LOG_LEVEL')
+logger.setLevel(logging.DEBUG)
+# don't really need to see this CRAP!
+logging.getLogger('discord.gateway').setLevel(logging.WARNING)
+
 for env_var in ['DISCORD_TOKEN', 'GUILD_ID', 'BOT_OWNERS']:
 	if not os.getenv(env_var):
-		print(f'ERROR: No value found in ${env_var}! Did you edit .env or compose.yaml?')
+		logger.error(f'No value defined for {env_var} ! Did you edit .env or compose.yaml?')
 		sys.exit(1)
-
-
-TOURNEY_DATA = None
-EPISODE_DATA = None
-
-with open('../data/structure.json', 'r', encoding='utf8') as sj:
-	TOURNEY_DATA = json.load(sj)
-
-with open('../data/episodes.json', 'r', encoding='utf8') as ej:
-	EPISODE_DATA = json.load(ej)
-
-
-# https://stackoverflow.com/a/65418112
-def find_key_nonrecursive(adict: dict, key: any) -> any:
-	stack = [adict]
-	while stack:
-		d = stack.pop()
-
-		if type(d) is list or type(d) is int:
-			continue
-
-		if key in d:
-			return d[key]
-		for v in d.values():
-			if isinstance(v, dict):
-				stack.append(v)
-			if isinstance(v, list):
-				stack += v
-
-
-class BracketBot(commands.Bot):
-	# We're only present in one guild so just sync commands on startup
-	async def setup_hook(self):
-		guild_id = int(os.getenv('GUILD_ID'))
-		target_guild = await self.fetch_guild(guild_id)
-
-		print(f'> Syncing commands to {target_guild.name} ({target_guild.id})...')
-
-		self.tree.copy_global_to(guild=target_guild)
-		await self.tree.sync(guild=target_guild)
-
-		print(f'> Commands synced to {target_guild.name} ({target_guild.id})!')
 
 
 def get_owner_ids() -> list[int]:
 	return list(map(int, os.getenv('BOT_OWNERS').split(',')))
 
 
-intents = discord.Intents.default()
-intents.message_content = True
+class BracketBot(commands.Bot):
+	def __init__(self):
+		intents = discord.Intents.default()
+		intents.message_content = True
 
-bot = BracketBot(command_prefix='b.', intents=intents, owner_ids=get_owner_ids())
+		super().__init__(
+			command_prefix=commands.when_mentioned_or('b.'),
+			intents=intents,
+			owner_ids=get_owner_ids(),
+			allowed_contexts=app_commands.AppCommandContext(guild=True, private_channel=False),
+			allowed_installs=app_commands.AppInstallationType(guild=True, user=False),
+			# TODO: Add description/help command
+			description=None,
+			help_command=None
+		)
+
+		self.read_tourney_data_file()
+		self.read_episode_data_file()
+
+	def read_tourney_data_file(self):
+		with open('../data/structure.json', 'r', encoding='utf8') as sj:
+			self._tourney_data = json.load(sj)
+			logger.debug('Tournament data loaded.')
+
+	def read_episode_data_file(self):
+		with open('../data/episodes.json', 'r', encoding='utf8') as ej:
+			self._episode_data = json.load(ej)
+			logger.debug('Episode data loaded.')
+
+	# We're only present in one guild so just sync commands on startup
+	async def setup_hook(self):
+		guild_id = int(os.getenv('GUILD_ID'))
+		target_guild = await self.fetch_guild(guild_id)
+
+		logger.debug(f'Syncing commands to {target_guild.name} ({target_guild.id})...')
+
+		self.tree.copy_global_to(guild=target_guild)
+		await self.tree.sync(guild=target_guild)
+
+		logger.info(f'Commands synced to {target_guild.name} ({target_guild.id}).')
+
+
+bot = BracketBot()
 
 
 @bot.event
 async def on_ready():
-	print(f'> logged in as {bot.user} ({bot.user.id})')
-
-
-@bot.tree.command(name='round', description='Display info about a round in the bracket')
-@app_commands.rename(round_id='round_number')
-@app_commands.describe(round_id='A specific round to inspect')
-async def cmd_round_info(
-	interaction: discord.Interaction,
-	round_id: Optional[app_commands.Range[int, 1, 141]] = None
-):
-	if not round_id:
-		await interaction.response.send_message('not implemented yet', ephemeral=True)
-		return
-
-	round_info = find_key_nonrecursive(TOURNEY_DATA, str(round_id))
-	winner_id = round_info['winner']
-
-	if winner_id == -1:
-		await interaction.response.send_message(f"Round {round_id} doesn't have a winner yet!")
-	else:
-		winner = EPISODE_DATA[str(winner_id)]
-
-		# TODO: turn this into a cool embed or whatever
-		msg = (
-			f"### __Info for Round {round_id}__\n"
-			"Winner: " + f"Episode {winner['episode']} - [{winner['title']}]({winner['link']})\n"
-			f"-# [_Click here to see the bracket online!_](https://bracket.podaboutli.st/#round-{round_id})"
-		)
-
-		await interaction.response.send_message(msg, suppress_embeds=True)
+	logger.info(f'logged in as {bot.user} ({bot.user.id})')
 
 
 async def main():
 	async with bot:
 		await bot.add_cog(Ping(bot))
+		await bot.add_cog(RoundInfo(bot))
 		await bot.start(os.getenv('DISCORD_TOKEN'))
 
 
 if __name__ == '__main__':
-	asyncio.run(main())
+	logger.debug('Firing up the bot!')
+	try:
+		asyncio.run(main())
+	except KeyboardInterrupt:
+		logger.warning('Caught Ctrl+C, exiting...')
+		sys.exit(0)
---
+++ bot/cogs/round_info.py
@@ -0,0 +1,69 @@
+import discord
+import logging
+from discord import app_commands
+from discord.ext import commands
+from typing import Optional
+
+
+logger = logging.getLogger(__name__)
+
+
+# TODO: Might wanna move all the logic for traversing my insane weird data
+# structure to its own class/file
+# https://stackoverflow.com/a/65418112
+def find_key_nonrecursive(the_dict: dict, key: any) -> any:
+	stack = [the_dict]
+	while stack:
+		d = stack.pop()
+
+		# python gets angry when we try to iterate over these
+		if type(d) is list or type(d) is int:
+			continue
+
+		if key in d:
+			return d[key]
+		for v in d.values():
+			if isinstance(v, dict):
+				stack.append(v)
+			if isinstance(v, list):
+				stack += v
+
+
+class RoundInfo(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+		if type(self.bot._tourney_data) is not dict:
+			logger.warning(f'self.bot._tourney_data is {type(self.bot._tourney_data)}!')
+
+		if type(self.bot._episode_data) is not dict:
+			logger.warning(f'self.bot._tourney_data is {type(self.bot._episode_data)}!')
+
+	@app_commands.command(name='round_info', description='Display info about a round in the bracket')
+	@app_commands.rename(round_id='round_number')
+	@app_commands.describe(round_id='A specific round to inspect')
+	async def cmd_round_info(
+		self,
+		interaction: discord.Interaction,
+		round_id: Optional[app_commands.Range[int, 1, 141]] = None
+	):
+		if not round_id:
+			await interaction.response.send_message('not implemented yet', ephemeral=True)
+			return
+
+		round_info = find_key_nonrecursive(self.bot._tourney_data, str(round_id))
+		winner_id = round_info['winner']
+
+		if winner_id == -1:
+			await interaction.response.send_message(f"Round {round_id} doesn't have a winner yet!")
+		else:
+			winner = self.bot._episode_data[str(winner_id)]
+
+			# TODO: turn this into a cool embed or whatever
+			msg = (
+				f"### __Info for Round {round_id}__\n"
+				"Winner: " + f"Episode {winner['episode']} - [{winner['title']}]({winner['link']})\n"
+				f"-# [_Click here to see the bracket online!_](https://bracket.podaboutli.st/#round-{round_id})"
+			)
+
+			await interaction.response.send_message(msg, suppress_embeds=True)
